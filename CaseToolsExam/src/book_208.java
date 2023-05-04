d3b2613d02669fa499b3b56734e9062262fa0f44
exam <exam@Bcalab4-m06>
2023-05-04 09:17:16 +0530

    first commit

---
+++ CaseToolsExam/src/book_208.java
@@ -0,0 +1,17 @@
+
+public class book_208 {
+	int page_num;
+	int total_pages;
+	
+	book_208(int total_pages)
+	{
+		this.total_pages= total_pages;
+		this.page_num=1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
