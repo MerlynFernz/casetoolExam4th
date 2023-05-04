ed29b2766ab7ffa8d06cacc29a87b23e20c53d61
exam <exam@Bcalab4-m06>
2023-05-04 09:26:43 +0530

    second commit

--- CaseToolsExam/src/book_208.java
+++ CaseToolsExam/src/book_208.java
@@ -8,6 +8,11 @@ public class book_208 {
 		this.total_pages= total_pages;
 		this.page_num=1;
 	}
+	int go_forward()
+	{
+		page_num++;
+		return page_num;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
